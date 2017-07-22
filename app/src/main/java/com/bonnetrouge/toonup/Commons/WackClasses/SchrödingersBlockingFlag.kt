71d5f3992456323374795a28971d0be880b75dd9
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2017-07-21 22:27:40 -0400

    Add wack SchrodingersBlockingFlag class

---
+++ app/src/main/java/com/bonnetrouge/toonup/Commons/WackClasses/SchrödingersBlockingFlag.kt
@@ -0,0 +1,19 @@
+package com.bonnetrouge.toonup.Commons.WackClasses
+
+class SchrödingersBlockingFlag(initBlocking: Boolean) {
+
+	var isBlocking: Boolean = initBlocking
+		private set(value) {
+			field = value
+		}
+	private var blockingCommutator: Int = if (initBlocking) 1 else 0
+
+	fun reduceBlock() {
+		if (blockingCommutator != 0) if (blockingCommutator-- == 1) isBlocking = false
+	}
+
+	fun addBlock() {
+		blockingCommutator++
+		isBlocking = true
+	}
+}
