928a8ff824cf6a1284a3f9c8745717d7c4497dba
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2017-08-05 12:36:14 -0400

    Add base PlayerActivity

---
+++ app/src/main/java/com/bonnetrouge/toonup/Activities/PlayerActivity.kt
@@ -0,0 +1,14 @@
+package com.bonnetrouge.toonup.Activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.bonnetrouge.toonup.R
+
+class PlayerActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_player)
+	}
+}
