1ce45d43b3cd6f71692db7d937191899b18d4b49
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2017-08-05 12:40:12 -0400

    WIP: Add fullscreen exoplayer to PlayerActivity

--- app/src/main/java/com/bonnetrouge/toonup/Activities/PlayerActivity.kt
+++ app/src/main/java/com/bonnetrouge/toonup/Activities/PlayerActivity.kt
@@ -1,11 +1,20 @@
 package com.bonnetrouge.toonup.Activities
 
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 import com.bonnetrouge.toonup.R
 
 class PlayerActivity : AppCompatActivity() {
+
+	companion object {
+		fun navigate(context: Context) {
+			val intent = Intent(context, PlayerActivity::class.java)
+			context.startActivity(intent)
+		}
+	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
