7297bec24a13967fa5c3529bc176e6e4f0b90819
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2017-09-23 09:59:00 -0400

    Add gradient animation for category chooser

--- app/src/main/java/com/bonnetrouge/toonup/Fragments/CategoryChooserFragment.kt
+++ app/src/main/java/com/bonnetrouge/toonup/Fragments/CategoryChooserFragment.kt
@@ -1,4 +1,5 @@
 package com.bonnetrouge.toonup.Fragments
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -11,13 +12,28 @@ import javax.inject.Inject
 
 class CategoryChooserFragment @Inject constructor(): Fragment() {
 
-	override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-		return inflater?.inflate(R.layout.fragment_category_chooser, container, false)
-	}
+	val backgroundAnimation by lazy { rootChooserLayout.background as AnimationDrawable }
+
+	override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+            inflater?.inflate(R.layout.fragment_category_chooser, container, false)
 
 	override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
+		with (backgroundAnimation) {
+            setEnterFadeDuration(5000)
+			setExitFadeDuration(5000)
+		}
 		tvShowsChooserContainer.setOnClickListener { (activity as BrowseActivity).navigateTvShows() }
 		moviesChooserContainer.setOnClickListener { (activity as BrowseActivity).navigateMovies() }
 	}
+
+    override fun onResume() {
+        super.onResume()
+        backgroundAnimation.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        backgroundAnimation.stop()
+    }
 }
