8b20d4a8abd3cdaa54afb4db67a0dc50e72e974e
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2017-10-17 11:05:28 -0400

    Add Delegates for BrowseViewModel

---
+++ app/src/main/java/com/bonnetrouge/toonup/Delegates/BrowseVMDelegates.kt
@@ -0,0 +1,17 @@
+package com.bonnetrouge.toonup.Delegates
+
+sealed class BrowseVMDelegate {
+
+}
+
+class CartoonsVMDelegate : BrowseVMDelegate() {
+
+}
+
+class MoviesVMDelegate : BrowseVMDelegate() {
+
+}
+
+class AnimeVMDelegate : BrowseVMDelegate() {
+
+}
