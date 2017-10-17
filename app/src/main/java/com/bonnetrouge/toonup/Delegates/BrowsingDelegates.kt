0b53f96de0b8f62cdb1cfa8d8b41382269db2c58
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2017-10-17 10:52:16 -0400

    Change delegate classes from an interface to a sealed class

--- app/src/main/java/com/bonnetrouge/toonup/Delegates/BrowsingDelegates.kt
+++ app/src/main/java/com/bonnetrouge/toonup/Delegates/BrowsingDelegates.kt
@@ -4,23 +4,23 @@ import com.bonnetrouge.toonup.Model.BannerModel
 import com.bonnetrouge.toonup.ViewModels.BrowseViewModel
 import io.reactivex.Observable
 
-interface DataFetchingDelegate {
-    fun fetchBrowsingData(loadingAction: () -> Unit, viewModel: BrowseViewModel, onSuccess: Observable<MutableList<BannerModel>>.() -> Unit, onFailure: () -> Unit)
+sealed class DataFetchingDelegate {
+    abstract fun fetchBrowsingData(loadingAction: () -> Unit, viewModel: BrowseViewModel, onSuccess: Observable<MutableList<BannerModel>>.() -> Unit, onFailure: () -> Unit)
 }
 
-class CartoonFetchingDelegate : DataFetchingDelegate {
+class CartoonFetchingDelegate : DataFetchingDelegate() {
     override fun fetchBrowsingData(loadingAction: () -> Unit, viewModel: BrowseViewModel, onSuccess: Observable<MutableList<BannerModel>>.() -> Unit, onFailure: () -> Unit) {
         viewModel.fetchCartoons(loadingAction, onSuccess, onFailure)
     }
 }
 
-class MovieFetchingDelegate : DataFetchingDelegate {
+class MovieFetchingDelegate : DataFetchingDelegate() {
     override fun fetchBrowsingData(loadingAction: () -> Unit, viewModel: BrowseViewModel, onSuccess: Observable<MutableList<BannerModel>>.() -> Unit, onFailure: () -> Unit) {
         viewModel.fetchMovies(loadingAction, onSuccess, onFailure)
     }
 }
 
-class AnimeFetchingDelegate : DataFetchingDelegate {
+class AnimeFetchingDelegate : DataFetchingDelegate() {
     override fun fetchBrowsingData(loadingAction: () -> Unit, viewModel: BrowseViewModel, onSuccess: Observable<MutableList<BannerModel>>.() -> Unit, onFailure: () -> Unit) {
         viewModel.fetchAnime(loadingAction, onSuccess, onFailure)
     }
--- app/src/main/java/com/bonnetrouge/toonup/Delegates/SearchDelegates.kt
+++ app/src/main/java/com/bonnetrouge/toonup/Delegates/SearchDelegates.kt
@@ -3,23 +3,23 @@ package com.bonnetrouge.toonup.Delegates
 import com.bonnetrouge.toonup.Model.BasicSeriesInfo
 import com.bonnetrouge.toonup.ViewModels.BrowseViewModel
 
-interface SearchDelegate {
-    fun getFilteredSearchResults(s: CharSequence, viewModel: BrowseViewModel, onResult: (MutableList<BasicSeriesInfo>) -> Unit)
+sealed class SearchDelegate {
+    abstract fun getFilteredSearchResults(s: CharSequence, viewModel: BrowseViewModel, onResult: (MutableList<BasicSeriesInfo>) -> Unit)
 }
 
-class CartoonSearchDelegate : SearchDelegate {
+class CartoonSearchDelegate : SearchDelegate() {
     override fun getFilteredSearchResults(s: CharSequence, viewModel: BrowseViewModel, onResult: (MutableList<BasicSeriesInfo>) -> Unit) {
         onResult(viewModel.getCartoonSearchResults(s))
     }
 }
 
-class MoviesSearchDelegate : SearchDelegate {
+class MoviesSearchDelegate : SearchDelegate() {
     override fun getFilteredSearchResults(s: CharSequence, viewModel: BrowseViewModel, onResult: (MutableList<BasicSeriesInfo>) -> Unit) {
         onResult(viewModel.getMoviesSearchResults(s))
     }
 }
 
-class AnimeSearchDelegate : SearchDelegate {
+class AnimeSearchDelegate : SearchDelegate() {
     override fun getFilteredSearchResults(s: CharSequence, viewModel: BrowseViewModel, onResult: (MutableList<BasicSeriesInfo>) -> Unit) {
         onResult(viewModel.getAnimeSearchResult(s))
     }
