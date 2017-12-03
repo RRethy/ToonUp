d97deb30ea41422977374216816f9bb8600ba447
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2017-12-03 00:43:13 -0500

    Make search filter pattern fuzzier: Don't match start of string

--- app/src/main/java/com/bonnetrouge/toonup/Commons/Ext/CollectionExt.kt
+++ app/src/main/java/com/bonnetrouge/toonup/Commons/Ext/CollectionExt.kt
@@ -2,10 +2,13 @@ package com.bonnetrouge.toonup.Commons.Ext
 
 import java.util.regex.Pattern
 
+val LOW_FUZZY = 1
+val MED_FUZZY = 2
+
 /**
  * Returns a list containing only elements where an elements [filterProperty] fuzzy matches [match]
  */
-inline fun <T> Iterable<T>.fuzzyFilter(fuzziness: Int = 1, match: CharSequence, filterProperty: T.() -> String): List<T> {
+inline fun <T> Iterable<T>.fuzzyFilter(fuzziness: Int = LOW_FUZZY, match: CharSequence, filterProperty: T.() -> String): List<T> {
     val lazySearchPattern = getFuzzinessPattern(fuzziness, match)
     val filteredList = mutableListOf<T>()
     this.forEach {
@@ -28,7 +31,8 @@ fun <T> Iterable<T>.getNext(current: T): T {
  */
 fun getFuzzinessPattern(fuzziness: Int, stringMatch: CharSequence): Pattern {
     when (fuzziness) {
-        1 -> return buildFuzziness1Pattern(stringMatch)
+        LOW_FUZZY -> return buildFuzziness1Pattern(stringMatch)
+        MED_FUZZY -> return buildFuzziness2Pattern(stringMatch)
         else -> return buildFuzziness1Pattern(stringMatch)
     }
 }
@@ -39,6 +43,20 @@ fun getFuzzinessPattern(fuzziness: Int, stringMatch: CharSequence): Pattern {
 fun buildFuzziness1Pattern(stringMatch: CharSequence): Pattern {
     val lazySearchRegexBuilder = StringBuilder()
     lazySearchRegexBuilder.append("^(")
+    stringMatch.forEach {
+        lazySearchRegexBuilder.append(it)
+        lazySearchRegexBuilder.append(".*")
+    }
+    lazySearchRegexBuilder.append(")")
+    return Pattern.compile(lazySearchRegexBuilder.toString(), Pattern.CASE_INSENSITIVE)
+}
+
+/**
+ * Builds a pattern for [stringMatch] that has a fuzziness factor of 1
+ */
+fun buildFuzziness2Pattern(stringMatch: CharSequence): Pattern {
+    val lazySearchRegexBuilder = StringBuilder()
+    lazySearchRegexBuilder.append("(")
     stringMatch.forEach {
         lazySearchRegexBuilder.append(it)
         lazySearchRegexBuilder.append(".*")
